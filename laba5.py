5655f7a3d656ab680b24c8d542bf14a67cab5132
Arsen <arsen.shulak@gmail.com>
2023-12-01 16:33:44 +0200

    Update laba5.py

--- laba5.py
+++ laba5.py
@@ -1,5 +1,5 @@
 from enum import Enum
-billion = 1000000000
+BILLION = 1000000000
 
 class GovernmentType(Enum):
     Демократія = 1
@@ -24,25 +24,18 @@ class Country:
     '''
     def get_name(self):
         return self.__name
-    
     def get_capital(self):
         return self.__capital
-    
     def get_code(self):
         return self.__code
-    
     def get_population(self):
         return self.__population
-    
     def get_area(self):
         return self.__area
-    
     def get_GDP(self):
         return self.__GDP
-    
     def get_government_type(self):
         return self.__government_type
-    
     def __str__(self):
         '''
             this fnc gives you info about selected country
@@ -93,7 +86,42 @@ class Land:
         '''
             This fnc returns every contry with population over one bilion
         '''
-        return [country for country in self.__countries if country.get_population() > billion]
+        return [country for country in self.__countries if country.get_population() > BILLION]
+    
+    def print_countries(self):
+        '''
+            This fnc prints info about every country defined by land
+        '''
+        print(f"\n\033[1;3m Країни у {self.get_name()}:\033[0m")
+        for country in self.get_countries():
+            print(country)
+
+    def print_population_density(self):
+        '''
+            This fnc prints info about population density of every country defined by land
+        '''
+        print(f"\n\033[1;3m Густина населення у кожній країні {self.get_name()}:\033[0m")
+        density_per_country = self.calculate_population_density()
+        for country_name, density in density_per_country:
+            print(f'{country_name}: {density:.1f} людей на квадратний метр')
+
+    def print_top_countries_by_gdp(self):
+        '''
+            This fnc prints sorted info about every country defined by land
+        '''
+        print("\n\033[1;3m Топ країн за ВВП:\033[0m")
+        top_countries = self.sort_countries_by_gdp()
+        for x, country in enumerate(top_countries):
+            print(f"{x + 1}. {country.get_name()} ({country.get_GDP()})")
+
+    def print_countries_by_population(self):
+        '''
+            This fnc prints every defined by land contry that have more than one billion population
+        '''
+        print("\n\033[1;3m Країни з населенням більше одного міліарда:\033[0m",)
+        selected_countries = self.select_country_by_population()
+        for country in selected_countries:
+            print(f'{country.get_name()} --- {country.get_population()} людей')
 
 
 def main():
@@ -115,24 +143,10 @@ def main():
     land3.add_country(country3)
     land3.add_country(country4)
 
-    print(f"\n\033[1;3m Країни у {land1.get_name()}:\033[0m")
-    for country in land1.get_countries():
-        print(country)
-
-    print(f"\n\033[1;3m Густина населення у кожній країні {land1.get_name()}:\033[0m")
-    density_per_country = land1.calculate_population_density()
-    for country_name, density in density_per_country:
-        print(f'{country_name}: {density:.1f} людей на квадратний метр')
-
-    print("\n\033[1;3m Топ країн за ВВП:\033[0m")
-    top_countries = land3.sort_countries_by_gdp()
-    for x, country in enumerate(top_countries):
-        print(f"{x + 1}. {country.get_name()} ({country.get_GDP()})")
-
-    print("\n\033[1;3m Країни з населенням більше одного міліарда:\033[0m",)
-    selected_countries = land3.select_country_by_population()
-    for country in selected_countries:
-        print(f'{country.get_name()} --- {country.get_population()} людей')
+    land1.print_countries()
+    land1.print_population_density()
+    land3.print_top_countries_by_gdp()
+    land3.print_countries_by_population()
 
 if __name__ == "__main__":
     main()
